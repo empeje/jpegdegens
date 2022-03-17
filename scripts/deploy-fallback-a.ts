9e13cb289070ddb60fc04a50a563c89ae906b94f
empeje <11813607+empeje@users.noreply.github.com>
2022-03-18 02:09:37 +0700

    Delegate call part 1

---
+++ scripts/deploy-fallback-a.ts
@@ -0,0 +1,33 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+async function deploy(name, ...args){
+    const Fallback = await ethers.getContractFactory(name);
+    const fallback = await Fallback.deploy(...args);
+    await fallback.deployed();
+
+    console.log(fallback.address);
+
+    return fallback;
+}
+
+async function fallback() {
+    const a = await deploy("A");
+    const b = await deploy("B", a.address)
+
+    console.log("A", await a.getA());
+    console.log("B", await b.getB());
+    console.log(".............");
+
+    await a.setA(42);
+    console.log("A", await a.getA());
+    console.log("B", await b.getB());
+    console.log(".............");
+
+    await b.setB(60);
+    console.log("A", await a.getA());
+    console.log("B", await b.getB());
+    console.log(".............");
+}
+
+fallback();
