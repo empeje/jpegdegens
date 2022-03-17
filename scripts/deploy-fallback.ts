c3df66fd38d10d8e0d2604b41f6bee6e53eeae70
empeje <11813607+empeje@users.noreply.github.com>
2022-03-18 01:52:14 +0700

    Try fallback

---
+++ scripts/deploy-fallback.ts
@@ -0,0 +1,19 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+async function deploy(){
+    const Fallback = await ethers.getContractFactory("Fallback");
+    const fallback = await Fallback.deploy();
+    await fallback.deployed();
+
+    console.log(fallback.address);
+
+    return fallback;
+}
+
+async function count(fallback) {
+    const f = await ethers.getContractAt("IFallback", fallback.address);
+    await f.count();
+}
+
+deploy().then(count);
